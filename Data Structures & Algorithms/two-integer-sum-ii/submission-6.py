7bdf92a786eea2a893595dbecf4b6d3f513023fb
Austin Zhang <austinzhangcy@gmail.com>
2026-05-26 09:52:55 +0800

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        while i < len(numbers):
+            residual = target - numbers[i]
+            j = 0
+            while j < len(numbers) and numbers[j] <= residual:
+                print(numbers[j], residual)
+                if numbers[j] == residual and i != j:
+                    return [i+1, j+1]
+                j += 1
+            i += 1
+        return [-1, -1]
